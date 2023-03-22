f6961bd3dba8e14213cbbf6803b1a3e2a7713e4f
chanil4129@naver.com <chanil4129@naver.com>
2023-03-22 18:04:12 +0900

    db

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-// require('dotenv').config(); // .env파일 필수
+require('dotenv').config(); // .env파일 필수
 const express = require('express');
 const cors = require('cors');
+const mysql2=require('./mysql/mysql_database');
 // const corsOptions = {
 //     origin: 'https://',
 //     credentials: true
@@ -11,9 +12,16 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors(/*corsOptions*/));
-app.get('/', (req, res) => res.sendFile(__dirname + '/test.html'));
+app.get('/', async (req, res) => {
+
+});
 app.get('/login', (req, res) => res.sendFile(__dirname + '/testLogin.html'));
 app.use('/api', userRoutes);
-
+(async function test() {
+    const q = `SELECT * FROM CSEY.test;`
+    const conn = await mysql2.getConnection();
+    const [ result ] = await conn.query(q);
+    console.log(result);
+})();
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
