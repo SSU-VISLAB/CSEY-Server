c7e8f417acca8db29ee263b89f5fbde9dda05eda
ptyoiy <56474564+ptyoiy@users.noreply.github.com>
2023-10-10 03:19:39 +0900

    feat: adminOptions 코드 분할, sidebar 로고 및 번역 추가

--- index.ts
+++ index.ts
@@ -1,13 +1,12 @@
 import AdminJSExpress from "@adminjs/express";
 import * as AdminJSSequelize from '@adminjs/sequelize';
-import AdminJS, { AdminJSOptions, ResourceWithOptions } from "adminjs";
+import AdminJS from "adminjs";
 import cors from "cors";
 import express, { json, urlencoded } from "express";
 import path from "path";
 import * as url from 'url';
-import { Components, componentLoader } from "./adminPage/components/index.ts";
-import { COMMON, EVENT, NOTICE } from "./adminPage/resources/index.ts";
-import { Alarm, Bookmark, BookmarkAsset, Event, EventsLike, Notice, NoticesLike, Read, ReadAsset, User, sequelize } from "./models/index.ts";
+import { adminOptions } from "./adminPage/index.ts";
+import { sequelize } from "./models/index.ts";
 import userRouter from "./routes/user.ts";
 
 // const corsOptions = {
@@ -31,61 +30,6 @@ AdminJS.registerAdapter({
 });
 
 const start = async () => {
-	const adminOptions: AdminJSOptions = {
-		dashboard: {
-			component: Components.Dashboard
-		},
-		// 번역(기본 - en)
-		locale: {
-			language: 'en',
-			translations: {
-				en: {
-					actions: {
-						list: 'list1',
-						search: 'search1',
-						new: '새로 만들기',
-						show: 'show1',
-						edit: 'edit1',
-						delete: 'delete1'
-					},
-					labels: {
-						users: '사용자',
-						alarms: '알림',
-						events_like: '사용자-행사',
-						notices_like: '사용자-공지',
-						events: '행사',
-						notices: '공지',
-					}
-				}
-			}
-		},
-		// list에서 페이지 별 표시될 행 수
-		settings: {
-			defaultPerPage: 5,
-		},
-		// 따로 추가할 정적 파일들 (css, js)
-		assets: {
-			styles: ["/event_show.css"]
-		},
-		// 관리할 models 목록
-		resources: [
-			// user
-			{ resource: User, options: COMMON.options },
-			{ resource: Alarm, options: COMMON.options },
-			{ resource: EventsLike, options: COMMON.options },
-			{ resource: NoticesLike, options: COMMON.options },
-			// post
-			{ resource: Event, options: EVENT.options, features: EVENT.features},
-			{ resource: Notice, options: NOTICE.options },
-			// others
-			{ resource: Read, options: COMMON.options },
-			{ resource: ReadAsset, options: COMMON.options },
-			{ resource: Bookmark, options: COMMON.options },
-			{ resource: BookmarkAsset, options: COMMON.options }
-		] as ResourceWithOptions[],
-		componentLoader
-	};
-	
 	const admin = new AdminJS(adminOptions);
 	admin.watch();
 	const adminRouter = AdminJSExpress.buildRouter(admin);
