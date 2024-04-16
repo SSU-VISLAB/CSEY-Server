07a95183716d1cf109fd6a715791e6221f6fe281
ptyoiy <56474564+ptyoiy@users.noreply.github.com>
2024-04-17 02:47:46 +0900

    fix: improve the completeness of model code

--- models/bookmark_assets.ts
+++ models/bookmark_assets.ts
@@ -1,6 +1,7 @@
 import { DataTypes } from 'sequelize';
-import { sequelize } from './sequelize.js';
 import Bookmark from './bookmarks.js';
+import Event from './events.js';
+import { sequelize } from './sequelize.js';
 
 const BookmarkAsset = sequelize.define('BookmarkAsset', {
   id: {
@@ -13,7 +14,7 @@ const BookmarkAsset = sequelize.define('BookmarkAsset', {
     type: DataTypes.BIGINT,
     allowNull: false,
     references: {
-      model: 'events',
+      model: Event,
       key: 'id',
     },
     onUpdate: "CASCADE",
@@ -23,7 +24,7 @@ const BookmarkAsset = sequelize.define('BookmarkAsset', {
     type: DataTypes.BIGINT,
     allowNull: false,
     references: {
-      model: 'bookmarks',
+      model: Bookmark,
       key: 'id'
     },
     onUpdate: "CASCADE",
@@ -33,8 +34,17 @@ const BookmarkAsset = sequelize.define('BookmarkAsset', {
   modelName: 'BookmarkAsset',
   tableName: 'bookmark_assets',
   timestamps: false, // createdAt 및 updatedAt 필드 생성 방지
+  indexes: [{
+    unique: true,
+    fields: ['fk_bookmark_id', 'fk_event_id']
+  }]
 });
 
-Bookmark.hasMany(BookmarkAsset, { as: 'BookmarkAssets', foreignKey: 'fk_bookmark_id' });
+// Event와 BookmarkAsset 간의 관계
+Event.hasMany(BookmarkAsset, { foreignKey: 'fk_event_id' });
+BookmarkAsset.belongsTo(Event, { foreignKey: 'fk_event_id' });
 
+// Bookmark와 BookmarkAsset 간의 관계
+Bookmark.hasMany(BookmarkAsset, { foreignKey: 'fk_bookmark_id' });
+BookmarkAsset.belongsTo(Bookmark, { foreignKey: 'fk_bookmark_id' });
 export default BookmarkAsset;
--- models/bookmarks.ts
+++ models/bookmarks.ts
@@ -1,7 +1,9 @@
 import { DataTypes } from 'sequelize';
 import { sequelize } from './sequelize.js';
+import { IBookmark } from './types.js';
+import User from './user.js';
 
-const Bookmark = sequelize.define('Bookmark', {
+const Bookmark = sequelize.define<IBookmark>('Bookmark', {
   id: {
     type: DataTypes.INTEGER.UNSIGNED,
     primaryKey: true,
@@ -12,7 +14,7 @@ const Bookmark = sequelize.define('Bookmark', {
     type: DataTypes.BIGINT,
     allowNull: false,
     references: {
-      model: 'users',
+      model: User,
       key: 'id',
     },
     onUpdate: "CASCADE",
@@ -23,5 +25,7 @@ const Bookmark = sequelize.define('Bookmark', {
   tableName: 'bookmarks',
   timestamps: false, // createdAt 및 updatedAt 필드 생성 방지
 });
-
+// User와 Bookmark 간의 관계
+User.hasMany(Bookmark, { foreignKey: 'fk_user_id' });
+Bookmark.belongsTo(User, { foreignKey: 'fk_user_id' });
 export default Bookmark;
